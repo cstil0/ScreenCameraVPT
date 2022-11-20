e2237d069344d3409a38372de3efba3940c27b8c
cstil <102225573+cstil0@users.noreply.github.com>
2022-11-20 23:42:18 +0100

    Fixing UDP

--- Assets/Scripts/Network/UDPReceiver.cs
+++ Assets/Scripts/Network/UDPReceiver.cs
@@ -1,14 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using System;
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
-using UnityEngine.UI;
-using System.IO;
 using System.Globalization;
+using System.Collections;
 
 public class UDPReceiver : MonoBehaviour
 {
@@ -16,7 +13,11 @@ public class UDPReceiver : MonoBehaviour
     UdpClient client;
     public int serverPort;
     Thread receiveThread;
+    Thread evaluateThread;
     String receivedMessage;
+    Queue messagesToEvaluate;
+
+    Messages lastMessageType;
 
     public Camera ScreenCamera;
     Vector3 startPos;
@@ -30,13 +31,17 @@ public class UDPReceiver : MonoBehaviour
     Quaternion remoteStartRot;
     //Vector3 currRot;
     Quaternion currRot;
+    Quaternion currSceneRot;
+
+    public RenderTexture screenTexture; 
 
     enum Messages
     {
-        RESET_START,
-        CAMERA_ROT,
-        CAMERA_POS,
-        SCENE_ROTATION
+        ALREADY_EVALUATED,
+        CAMERA_INFO,
+        SCENE_ROTATION,
+        SEND_NDI,
+        SEND_DISPLAY
     }
     // main thread that listens to UDP messages through a defined port
     void ReceiveUDP()
@@ -53,7 +58,75 @@ public class UDPReceiver : MonoBehaviour
                 byte[] receiveBytes = client.Receive(ref remoteEndPoint);
                 // once the message is recieved, encode it as ASCII
                 receivedMessage = Encoding.ASCII.GetString(receiveBytes);
-                evaluateMessage(receivedMessage);
+
+                Messages message_enum = 0;
+                string message = "";
+
+                if (receivedMessage.Contains(":"))
+                {
+                    string[] splittedMessageType = receivedMessage.Split(":".ToCharArray());
+                    // convert string to correct enum type
+                    message_enum = (Messages)Enum.Parse(typeof(Messages), splittedMessageType[0]);
+                    message = splittedMessageType[1];
+                }
+                else
+                {
+                    message_enum = (Messages)Enum.Parse(typeof(Messages), receivedMessage);
+                    message = receivedMessage;
+                }
+
+
+                // switch action according to message
+                switch (message_enum)
+                {
+                    case Messages.CAMERA_INFO:
+                        resetStart = bool.Parse(message);
+
+                        // Receive positition
+                        receiveBytes = client.Receive(ref remoteEndPoint);
+                        // once the message is recieved, encode it as ASCII
+                        receivedMessage = Encoding.ASCII.GetString(receiveBytes);
+
+                        int parenthesisIndex = receivedMessage.IndexOf(")");
+                        string filteredMessage = receivedMessage.Substring(1, parenthesisIndex - 1);
+                        //string filteredMessage = receivedMessage.Substring(1, -2);
+                        string[] splittedMessage = filteredMessage.Split(", ".ToCharArray());
+                        currPos = new Vector3(float.Parse(splittedMessage[0], CultureInfo.InvariantCulture), float.Parse(splittedMessage[2], CultureInfo.InvariantCulture), float.Parse(splittedMessage[4], CultureInfo.InvariantCulture));
+
+                        // Receive rotation
+                        receiveBytes = client.Receive(ref remoteEndPoint);
+                        // once the message is recieved, encode it as ASCII
+                        receivedMessage = Encoding.ASCII.GetString(receiveBytes);
+
+                        //splittedMessage = receivedMessage.Split(" ");
+                        parenthesisIndex = receivedMessage.IndexOf(")");
+                        filteredMessage = receivedMessage.Substring(1, parenthesisIndex - 1);
+                        splittedMessage = filteredMessage.Split(", ".ToCharArray());
+
+                        currRot = new Quaternion(float.Parse(splittedMessage[0], CultureInfo.InvariantCulture), float.Parse(splittedMessage[2], CultureInfo.InvariantCulture), float.Parse(splittedMessage[4], CultureInfo.InvariantCulture), float.Parse(splittedMessage[6], CultureInfo.InvariantCulture));
+
+                        lastMessageType = Messages.CAMERA_INFO;
+                        break;
+
+                    case Messages.SCENE_ROTATION:
+                        parenthesisIndex = message.IndexOf(")");
+                        filteredMessage = message.Substring(1, parenthesisIndex - 1);
+                        splittedMessage = filteredMessage.Split(", ".ToCharArray());
+
+                        currSceneRot = new Quaternion(float.Parse(splittedMessage[0], CultureInfo.InvariantCulture), float.Parse(splittedMessage[2], CultureInfo.InvariantCulture), float.Parse(splittedMessage[4], CultureInfo.InvariantCulture), float.Parse(splittedMessage[6], CultureInfo.InvariantCulture));
+
+                        lastMessageType = Messages.SCENE_ROTATION;
+                        break;
+
+                    case Messages.SEND_NDI:
+                        lastMessageType = Messages.SEND_NDI;
+                        break;
+
+                    case Messages.SEND_DISPLAY:
+                        lastMessageType = Messages.SEND_DISPLAY;
+                        break;
+
+                }
             }
             catch (Exception e)
             {
@@ -62,89 +135,120 @@ public class UDPReceiver : MonoBehaviour
         }
     }
 
-    void evaluateMessage(String receivedMessage)
+
+    void changePos()
     {
-        string[] splittedMessageType = receivedMessage.Split(":".ToCharArray());
-        // convert string to correct enum type
-        Messages message_enum = (Messages)Enum.Parse(typeof(Messages), splittedMessageType[0]);
-        string message = splittedMessageType[1];
+        if (resetStart)
+            remoteStartPos = new Vector3(currPos.x, currPos.y, currPos.z);
 
-        // switch action according to message
-        switch (message_enum)
-        {
-            case Messages.RESET_START:
-                resetStart = bool.Parse(message);
-                break;
+        Vector3 remotePosDiff = remoteStartPos - currPos;
+        //Vector3 remotePosDiff = currPos - remoteStartPos;
+        ScreenCamera.transform.position = remotePosDiff + startPos;
 
-            case Messages.CAMERA_POS:
-                int parenthesisIndex = message.IndexOf(")");
-                string filteredMessage = message.Substring(1, parenthesisIndex - 1);
-                //string filteredMessage = receivedMessage.Substring(1, -2);
-                string[] splittedMessage = filteredMessage.Split(", ".ToCharArray());
-                currPos = new Vector3(float.Parse(splittedMessage[0], CultureInfo.InvariantCulture), float.Parse(splittedMessage[2], CultureInfo.InvariantCulture), float.Parse(splittedMessage[4], CultureInfo.InvariantCulture));
-
-                if (resetStart)
-                    remoteStartPos = new Vector3(currPos.x, currPos.y, currPos.z);
-
-                Vector3 remotePosDiff = currPos - remoteStartPos;
-                ScreenCamera.transform.position = remotePosDiff + startPos;
-
-                break;
-
-            case Messages.CAMERA_ROT:
-                //splittedMessage = receivedMessage.Split(" ");
-                parenthesisIndex = message.IndexOf(")");
-                filteredMessage = message.Substring(1, parenthesisIndex - 1);
-                splittedMessage = filteredMessage.Split(", ".ToCharArray());
-
-                currRot = new Quaternion(float.Parse(splittedMessage[0], CultureInfo.InvariantCulture), float.Parse(splittedMessage[2], CultureInfo.InvariantCulture), float.Parse(splittedMessage[4], CultureInfo.InvariantCulture), float.Parse(splittedMessage[6], CultureInfo.InvariantCulture));
-
-                if (resetStart)
-                    remoteStartRot = new Quaternion(currRot.x, currRot.y, currRot.z, currRot.w);
-
-                Quaternion remoteRotDiff = remoteStartRot * Quaternion.Inverse(currRot);
-                ScreenCamera.transform.rotation = remoteRotDiff * startRot;
-
-                break;
-
-            case Messages.SCENE_ROTATION:
-                parenthesisIndex = message.IndexOf(")");
-                filteredMessage = message.Substring(1, parenthesisIndex - 1);
-                splittedMessage = filteredMessage.Split(", ".ToCharArray());
-
-                currRot = new Quaternion(float.Parse(splittedMessage[0], CultureInfo.InvariantCulture), float.Parse(splittedMessage[2], CultureInfo.InvariantCulture), float.Parse(splittedMessage[4], CultureInfo.InvariantCulture), float.Parse(splittedMessage[6], CultureInfo.InvariantCulture));
-
-                Quaternion sceneRotDiff = remoteStartRot * Quaternion.Inverse(currRot);
-                ScreenCamera.transform.rotation = sceneRotDiff * startRot;
-
-                break;
-        }
     }
+    void changeRot()
+    {
+        if (resetStart)
+            remoteStartRot = new Quaternion(currRot.x, currRot.y, currRot.z, currRot.w);
+
+        Quaternion remoteRotDiff = currRot * Quaternion.Inverse(remoteStartRot);
+        //Quaternion remoteRotDiff = remoteStartRot * Quaternion.Inverse(currRot);
+        ScreenCamera.transform.rotation = remoteRotDiff * startRot;
+
+        lastMessageType = Messages.ALREADY_EVALUATED;
+    }
+
+    void rotateScene()
+    {
+        Quaternion sceneRotDiff = remoteStartRot * Quaternion.Inverse(currSceneRot);
+        ScreenCamera.transform.rotation = sceneRotDiff * startRot;
+        lastMessageType = Messages.ALREADY_EVALUATED;
+    }
+
+    void changeToNDI()
+    {
+        GameObject.Find("NDI Sender").SetActive(true);
+        ScreenCamera.targetTexture = screenTexture;
+        lastMessageType = Messages.ALREADY_EVALUATED;
+
+    }
+
+    void changeToDisplay()
+    {
+        GameObject.Find("NDI Sender").SetActive(false);
+        ScreenCamera.targetTexture = null;
+        ScreenCamera.targetDisplay = 0;
+        lastMessageType = Messages.ALREADY_EVALUATED;
+    }
+
+    //void evaluateMessage(String messageToEvaluate)
+    //{
+    //    Debug.Log(messageToEvaluate);
+    //}
+
+    //void readMessagesToEvaluate()
+    //{
+    //    while (true)
+    //    {
+    //        try
+    //        {
+    //           // get the first message that was added, evaluate it and eliminate it from the queue
+    //           evaluateMessage(messagesToEvaluate.Dequeue().ToString());
+    //        }
+    //        catch (Exception e) {}
+    //    }
+    //} 
 
     void OnDisable()
     {
         // stop thread when object is disabled
         if (receiveThread != null)
             receiveThread.Abort();
+
+        if (evaluateThread != null)
+            evaluateThread.Abort();
+
         client.Close();
     }
     // Start is called before the first frame update
     void Start()
     {
+        //messagesToEvaluate = new Queue();
+        lastMessageType = Messages.ALREADY_EVALUATED;
         resetStart = false;
+
         // Start thread to listen UDP messages and set it as background
         receiveThread = new Thread(ReceiveUDP);
         receiveThread.IsBackground = true;
         receiveThread.Start();
+
+        // Start thread to read and evaluate the messages and set it as background
+        //evaluateThread = new Thread(readMessagesToEvaluate);
+        //evaluateThread.IsBackground = true;
+        //evaluateThread.Start();
 
         startPos = ScreenCamera.transform.position;
         //startRot = ScreenCamera.transform.rotation.eulerAngles;
         startRot = ScreenCamera.transform.rotation;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-
+        switch (lastMessageType)
+        {
+            case Messages.CAMERA_INFO:
+                changePos();
+                changeRot();
+                break;
+            case Messages.SCENE_ROTATION:
+                rotateScene();
+                break;
+            case Messages.SEND_NDI:
+                changeToNDI();
+                break;
+            case Messages.SEND_DISPLAY:
+                changeToDisplay();
+                break;
+        }
     }
 }
